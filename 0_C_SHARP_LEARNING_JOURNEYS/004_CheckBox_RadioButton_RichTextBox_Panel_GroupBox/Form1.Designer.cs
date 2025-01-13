b97f37ae0ad211336b4ffe00e5c19f326237af65
giraydorukyurt7 <giraysoftwareengineer3.14@hotmail.com>
2025-01-13 11:34:18 +0300

    C# Learning - 4 (Form Applications)

---
+++ 0_C_SHARP_LEARNING_JOURNEYS/004_CheckBox_RadioButton_RichTextBox_Panel_GroupBox/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace _004_CheckBox_RadioButton_RichTextBox_Panel_GroupBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new CheckBox();
+            groupBox1 = new GroupBox();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            groupBox2 = new GroupBox();
+            radioButton4 = new RadioButton();
+            panel1 = new Panel();
+            radioButton5 = new RadioButton();
+            radioButton6 = new RadioButton();
+            richTextBox1 = new RichTextBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(20, 26);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(85, 24);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Ketchup";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(192, 255, 255);
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Location = new Point(12, 28);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(250, 187);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Sauces";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(20, 112);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(85, 24);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "Mustard";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(20, 67);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(108, 24);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Mayonaisse";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 26);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(60, 24);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Beef";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(6, 67);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(81, 24);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Chicken";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(-36, -25);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(117, 24);
+            radioButton3.TabIndex = 5;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.FromArgb(192, 255, 255);
+            groupBox2.Controls.Add(radioButton4);
+            groupBox2.Controls.Add(radioButton1);
+            groupBox2.Controls.Add(radioButton2);
+            groupBox2.Location = new Point(287, 28);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(250, 187);
+            groupBox2.TabIndex = 7;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Meat";
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(6, 111);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(62, 24);
+            radioButton4.TabIndex = 5;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Ham";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Brown;
+            panel1.Controls.Add(radioButton5);
+            panel1.Controls.Add(radioButton6);
+            panel1.Location = new Point(12, 238);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(250, 125);
+            panel1.TabIndex = 8;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(26, 18);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(63, 24);
+            radioButton5.TabIndex = 9;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Male";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(26, 80);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(78, 24);
+            radioButton6.TabIndex = 0;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "Female";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(293, 238);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(155, 120);
+            richTextBox1.TabIndex = 9;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.IndianRed;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Controls.Add(panel1);
+            Controls.Add(groupBox2);
+            Controls.Add(radioButton3);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private GroupBox groupBox1;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private GroupBox groupBox2;
+        private RadioButton radioButton4;
+        private Panel panel1;
+        private RadioButton radioButton5;
+        private RadioButton radioButton6;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ 0_C_SHARP_LEARNING_JOURNEYS/004_CheckBox_RadioButton_RichTextBox_Panel_GroupBox/Form1.cs
@@ -0,0 +1,20 @@
+namespace _004_CheckBox_RadioButton_RichTextBox_Panel_GroupBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
