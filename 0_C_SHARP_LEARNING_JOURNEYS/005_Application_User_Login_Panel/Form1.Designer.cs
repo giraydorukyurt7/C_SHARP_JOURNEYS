edbac27f2db34b3ab468b536cd7a73af010d053d
giraydorukyurt7 <giraysoftwareengineer3.14@hotmail.com>
2025-01-13 12:11:40 +0300

    C# Learning - 5 (Form Applications) - Login Panel

---
+++ 0_C_SHARP_LEARNING_JOURNEYS/005_Application_User_Login_Panel/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace _005_Application_User_Login_Panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            panel2 = new Panel();
+            label5 = new Label();
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(61, 114);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 26);
+            label1.TabIndex = 0;
+            label1.Text = "User Id:";
+            label1.Click += label1_Click;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(154, 111);
+            maskedTextBox1.Mask = "00000000000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(141, 34);
+            maskedTextBox1.TabIndex = 1;
+            maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(154, 165);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(141, 34);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(39, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 26);
+            label2.TabIndex = 3;
+            label2.Text = "Password:";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(192, 192, 255);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(0, -2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(587, 66);
+            panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(398, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(88, 59);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Viner Hand ITC", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(50, 11);
+            label3.Name = "label3";
+            label3.Size = new Size(315, 48);
+            label3.TabIndex = 0;
+            label3.Text = "Online lecture system";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(192, 192, 255);
+            panel2.Controls.Add(label5);
+            panel2.Location = new Point(0, 343);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(694, 37);
+            panel2.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.Location = new Point(12, 11);
+            label5.Name = "label5";
+            label5.Size = new Size(337, 28);
+            label5.TabIndex = 0;
+            label5.Text = "Project is only for design purposes";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(201, 205);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 33);
+            button1.TabIndex = 6;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(318, 167);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(192, 30);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Forgot Password";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 26F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 224, 192);
+            ClientSize = new Size(537, 379);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(label1);
+            Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 4, 5, 4);
+            Name = "Form1";
+            Text = "User Login Panel";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private MaskedTextBox maskedTextBox1;
+        private TextBox textBox1;
+        private Label label2;
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Button button1;
+        private CheckBox checkBox1;
+        private Label label5;
+    }
+}
---
+++ 0_C_SHARP_LEARNING_JOURNEYS/005_Application_User_Login_Panel/Form1.cs
@@ -0,0 +1,15 @@
+namespace _005_Application_User_Login_Panel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
